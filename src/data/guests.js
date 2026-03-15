105e270a38b4df93f714bb0711032ef49a9dbbe0
Jaicob <jaicob@icloud.com>
2026-03-14 17:57:13 -0700

    Add guest links

--- src/data/guests.js
+++ src/data/guests.js
@@ -2,12 +2,49 @@
  * Guest personalizations for invite links.
  *
  * Usage: /#/invite?g=<key>
- * Example: /#/invite?g=kim-family → "Dear Kim Family,"
+ * Example: /#/invite?g=hyunsub-choi → "Dear Choi Family,"
  *
  * Keys should be URL-friendly (lowercase, hyphens).
  * Values are the display text inserted after "Dear".
  */
 export const guests = {
-  'choi-family': 'Choi Family',
-  'kim': 'Katie & Eric',
+  'hyunsub-choi': 'Choi Family',
+  'timothy-choi': 'Timoti, Minkyung and Theo',
+  'sunny-hills': 'Sunny',
+  'katie-kim': 'Katie & Eric',
+  'jihye-park': 'Jihye',
+  'minjung-cho': 'Minjung Cho',
+  'sang-a-lee': 'Sang-a Lee',
+  'ken-han': 'Ken',
+  'mike-mccormick': 'Mike & Amanda',
+  'jiin-park': 'Jiin',
+  'saurabh-sharma': 'Saurabh & Molly',
+  'advait-vaishampayan': 'Advait & Samruddhi',
+  'ryan-chernis': 'Ryan',
+  'scott-dario': 'Scott',
+  'suzana-howell': 'Suzana & Andrew',
+  'carey-stewart': 'Carey Stewart',
+  'laura-martinez': 'Aunt Laura & Uncle Johnny',
+  'eugene-yukin': 'Eugene',
+  'marilyn-stewart': 'Grandma',
+  'caleb-adams': 'Caleb & Sarah',
+  'mary-frankel': 'Mary & Max',
+  'jay-deng': 'Jay & Michelle',
+  'jesse-zheng': 'Jesse',
+  'tom-macmicheal': 'Tom & Daisy',
+  'nicole-shum': 'Nicole',
+  'john-hsu': 'John',
+  'matthew-encina': 'Matthew & Belinda',
+  'xris-tjakra': 'Xris',
+  'andrew-casella': 'Andrew',
+  'valerie-riccardi': 'Valerie',
+  'andrei-secasiu': 'Andrei',
+  'nikhil-gupta': 'Nik & Claire',
+  'melissa-shaw': 'Shaw Family',
+  'oscar-johnson': 'Johnson Family',
+  'travis-stewart': 'Stewart Family',
+  'garret-stewart': 'Stewart Family',
+  'john-wes-martinez': 'John Wes & Michelle',
+  'dan-ahn': 'Dan & Nina',
+  'joon-lee': 'Joon & Michelle'
 };
